c8614a7de571bd67904fc6a7667043020bd2349b
uodeeb <uodeeb@gmail.com>
2018-07-30 05:07:11 +0200

    Suite01: add spec01 test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,7 +3,16 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
+/*_________________________suite 01 _______________________*/
+describe('RSS Feeds', function() {
+    /*_________________________spec 01 _______________________*/
+        it('are defined', function() {
+            // check if allfeeds is defined
+            expect(allFeeds).toBeDefined();
+            // check if allfeeds is not empty
+            expect(allFeeds.length).not.toBe(0);
 
+        });
 
 
 
