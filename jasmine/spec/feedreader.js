109fd7f90b51be6145c2c1d95b5f7e1d0960ba7e
uodeeb <uodeeb@gmail.com>
2018-07-31 11:38:09 +0200

    2nd modification to meet project rubics

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,9 +3,11 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-/*_________________________suite 01 _______________________*/
-describe('RSS Feeds', function() {
-    /*_________________________spec 01 _______________________*/
+
+    /*_________________________suite 01 _______________________*/
+    describe('RSS Feeds', function() {
+    
+        /*_________________________spec 01 _______________________*/
         it('are defined', function() {
             // check if allfeeds is defined
             expect(allFeeds).toBeDefined();
@@ -13,87 +15,112 @@ describe('RSS Feeds', function() {
             expect(allFeeds.length).not.toBe(0);
 
         });
-         /*_________________________spec 02 _______________________*/
-         it('has url', function() {
+
+    /*_________________________spec 02 _______________________*/
+        it('has url', function() {
             // check if url are defined into allfeeds
-            expect(allFeeds.keys('url')).toBeDefined();
+            //expect(allFeeds.keys('url')).toBeDefined();
             // check if url is not empty
-            expect(allFeeds.url).not.toBe('');
-            
+            //expect(allFeeds.url).not.toBe('');
+            allFeeds.filter((key)=>
+            expect(key.url).toBeGreaterThan('')
+            )
+            allFeeds.filter((key)=>
+            expect(key.url).toBeDefined()
+            )
         });
-/*_________________________spec 03 _______________________*/
-it('has a name', function() {
-    // check if name are defined into allfeeds
-    expect(allFeeds.keys('name')).toBeDefined();
-    // check if name is not empty
-    expect(allFeeds.name).not.toBe('');
+
+    /*_________________________spec 03 _______________________*/
+        it('has a name', function() {
+            // check if name are defined into allfeeds
+            allFeeds.filter((key)=>
+            expect(key.name).toBeGreaterThan('')
+            )
+            // check if name is not empty
+            allFeeds.filter((key)=>
+            expect(key.name).toBeDefined()
+            )
+            
+        })
+    });
+
+    /*_________________________suite 02 _______________________*/
+    describe('The menu', function() {
     
-})
-});
-/*_________________________suite 02 _______________________*/
-describe('The menu', function() {
-    /*_________________________spec 01 _______________________*/
-    it('is menu hidden', function() {
+        /*_________________________spec 01 _______________________*/
+        it('is menu hidden', function() {
         //check if menu has a default of hidden
         expect(document.body.classList).toContain('menu-hidden');
         
         }) 
-/*_________________________spec 02 _______________________*/
-it('is menu hidden when menu icon clicked', function() {
-    //selecting the icon click
-    $(".menu-icon-link").click();
-   //check if menu do not have a hidden class onclick
-      expect(document.body.classList).not.toContain('menu-hidden')
-   //selecting the icon click
-   $(".menu-icon-link").click();
-  //check if menu have a hidden class onclick  
-      expect(document.body.classList).toContain('menu-hidden')
 
-       
-   })
-})
+        /*_________________________spec 02 _______________________*/
+        it('is menu hidden when menu icon clicked', function() {
+         //selecting the icon click
+         $(".menu-icon-link").click();
+        //check if menu do not have a hidden class onclick
+           expect(document.body.classList).not.toContain('menu-hidden')
+        //selecting the icon click
+        $(".menu-icon-link").click();
+       //check if menu have a hidden class onclick  
+           expect(document.body.classList).toContain('menu-hidden')
+    
+            
+        })
+    })
+
 /*_________________________suite 03 _______________________*/
-describe('Initial Entries', function(){
+    describe('Initial Entries', function(){
+    
     /*_________________________spec 01 _______________________*/
-        // using done with beforeEach to check the asy function
+    // using done with beforeEach to check the asy function
         beforeEach(function(done){
-            loadFeed(0,function(){
-                done();
-            });
+        loadFeed(0,done);
         });
-        // check if loadFeed doing its job
+    // check if loadFeed doing its job
+
+        it('loadFeed complets its work',function() {
+        let entry = $('.feed .entry');
+        console.log(entry)
+        expect(entry).toBeDefined();
+        // another way to do this
+        /*
         it('loadFeed complets its work',function(done) {
         let entry = $('.feed .entry')[0];
         expect(entry).toBeGreaterThan('');
         done();
-        });
+         */
     
-    })
-    /*_________________________suite 04 _______________________*/
-describe('New Feed Selection', function(){
-    /*_________________________spec 01 _______________________*/
-    beforeEach(function(done){
+    });
 
-        loadFeed(0, function(){
+})
+
+/*_________________________suite 04 _______________________*/
+    describe('New Feed Selection', function(oldFeed, newFeed){
+    
+    /*_________________________spec 01 _______________________*/
+        beforeEach(function(done){
+
+            loadFeed(0, function(){
         
             oldfeed =$('.feed').html();
         
-            loadFeed(1, function() {
+                loadFeed(1, function() {
         
                   newFeed =$('.feed').html();       
         
                    done(); // call done when variables are fed and tests to begin
-              });
-         });
+                });
+            });
         });
 
-    it('loadFeed complets its work & content actually changes',function(done) {
-        // selecting the feed list to monitor content change
-        let feed = document.querySelector('.feed');
-        // check if the list content(enteries) are there
-        expect(feed.children.length).toBeGreaterThan(0);
+        it('loadFeed complets its work & content actually changes',function(done) {
+       
+        // check if the list content(enteries) are changed
+        
+        expect(newFeed).not.toBe(oldFeed);
         done();
-    });
+        });
 
-})
+    })
 }());
